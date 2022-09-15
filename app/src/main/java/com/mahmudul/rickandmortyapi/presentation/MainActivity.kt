d43ea5fd76cce70ef65b2ec7aadc7d4481d425fc
mhasancse15 <mhasancse15@gmail.com>
2022-09-15 23:03:05 +0600

    navigation implement

--- app/src/main/java/com/mahmudul/rickandmortyapi/presentation/MainActivity.kt
+++ app/src/main/java/com/mahmudul/rickandmortyapi/presentation/MainActivity.kt
@@ -2,11 +2,27 @@ package com.mahmudul.rickandmortyapi.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.mahmudul.rickandmortyapi.R
+import com.mahmudul.rickandmortyapi.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
--- app/src/main/java/com/mahmudul/rickandmortyapi/presentation/fragments/CharactersFragment.kt
+++ app/src/main/java/com/mahmudul/rickandmortyapi/presentation/fragments/CharactersFragment.kt
@@ -2,16 +2,16 @@ package com.mahmudul.rickandmortyapi.presentation.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.mahmudul.rickandmortyapi.R
 import com.mahmudul.rickandmortyapi.databinding.FragmentCharactersBinding
 import com.mahmudul.rickandmortyapi.domain.adapters.CharacterAdapter
 import com.mahmudul.rickandmortyapi.presentation.viewmodels.CharactersViewModel
 import com.mahmudul.rickandmortyapi.util.autoCleared
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharactersFragment : Fragment(R.layout.fragment_characters) {
 
     private var binding: FragmentCharactersBinding by autoCleared()
